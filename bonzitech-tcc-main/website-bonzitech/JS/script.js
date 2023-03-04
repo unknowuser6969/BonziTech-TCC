44cb746028c5bc8f161f8095ec8f449a324dbc07
Tulio <91757295+TulioTheGreat@users.noreply.github.com>
2023-03-04 19:15:28 -0300

    Add files via upload

---
+++ bonzitech-tcc-main/website-bonzitech/JS/script.js
@@ -0,0 +1,38 @@
+const sobreNosContainer = document.getElementById("sobre-nos-container");
+const sobreNosBtn = document.getElementById("sobre-nos-btn");
+const textosSobreNos = document.getElementById("sobre-nos-textos");
+const nossaEquipeDiv = document.getElementById("nossa-equipe");
+
+sobreNosBtn.addEventListener("click", () => {
+    const textoMaisSobreNosAtivo = textosSobreNos.childElementCount > 2;
+
+    if (textoMaisSobreNosAtivo) {
+        textosSobreNos.children[2].remove();
+        sobreNosContainer.style.margin = "0px";
+        nossaEquipeDiv.style.marginBottom = "0px";
+    }
+    else {
+        const maisSobreNos = document.createElement("p");
+
+        maisSobreNos.innerHTML = `
+        A BonziTech é uma pequena equipe, mas que sonha grande. <br> Nosso objetivo 
+        é facilitar seu trabalho da forma mais tecnológica e acessível possível, 
+        desenvolvendo tudo o que você pode imaginar e mais um pouco! <br> <br>
+
+        Desenvolvemos sistemas de computador, aplicativos desktop ou mobile e websites 
+        para qualquer tipo de negócio, tudo de forma ágil e com um plano que se adapte 
+        ao seu negócio. <b> Se pode ser feito, nós fazemos! </b> <br> <br>
+
+        BonziTech, trabalhando em seu negócio com você
+        `;
+
+        textosSobreNos.appendChild(maisSobreNos);
+        sobreNosContainer.style.marginBottom = "20vh";
+        nossaEquipeDiv.style.marginBottom = "40vh";
+    }
+
+    sobreNosBtn.textContent = 
+    (sobreNosBtn.textContent.includes("Saiba mais") 
+    ? "Mostrar menos" 
+    : "Saiba mais");
+});
